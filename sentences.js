383e628543fcf96498caedd94ccbdf7ee37ab3bb
Giu-Kun <110615330+Giu-Kun@users.noreply.github.com>
2023-12-18 10:54:46 +0100

    Create sentences.js

---
+++ sentences.js
@@ -0,0 +1,8 @@
+// sentences.js
+var sentences = [
+  "Stay positive and keep going!",
+  "Every moment is a fresh beginning.",
+  "Believe in yourself and all that you are.",
+  "Cherish each moment.",
+  "Life is short, make it sweet."
+];
